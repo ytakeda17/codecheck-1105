f27146c8151549f6308ef06a86f9b78bfdedf3dc
ytakeda17 <puz.sf5@gmail.com>
2016-02-19 20:47:23 +0900

    1st

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require("http");
+var url = require("url");
+
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log(request["method"]+pathname + " received.");
+    route(handle, pathname, response, request);
+  }
+
+  http.createServer(onRequest).listen(process.env.PORT||8888);
+  console.log("Server has started.");
+}
+
+exports.start = start;
