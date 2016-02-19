aa6ae5f6e33ef33a254a6ac964e38af7a7ac5957
ytakeda17 <puz.sf5@gmail.com>
2016-02-19 21:38:53 +0900

    2nd

--- main.js
+++ main.js
@@ -5,23 +5,26 @@ var
   port = process.env.PORT || 3000,
   parser = require('body-parser');
 
+app.use(parser.urlencoded({encoded:false}));
 app.use(parser.json());
 app.use(express.static(__dirname + '/public'))
 
-app.get('/api/ping', function (req, res, next) {
-  res.json('PONG');
+
+app.get('/api/projects', function (req, res, next) {
+  res.status(200).json('koujichu');
+  return next();
+});
+ 
+app.post('/api/projects/', function (req, res, next) {
+  res.status(200).json('BadRe');
   return next();
 });
 
-app.get('/api/notfound', function (req, res, next) {
-  res.status(404).json('NotFound');
+app.get('/api/projects/:id', function (req, res, next) {
+  var id = req.params.id;  
+  res.status(200).json(id);
   return next();
 });
-
-app.post('/api/badrequest', function (req, res, next) {
-  res.status(400).json('BadRequest');
-  return next();
-})
 
 app.listen(port, function () {
   console.log('Server running with port', port);
